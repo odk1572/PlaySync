4d97bc0aec4cf8a3b2b077cf055c1894003b8e1e
Om Kariya <161995427+odk1572@users.noreply.github.com>
2024-12-26 11:57:25 +0530

    Update user.routes.js (watch-history routes added)

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -14,6 +14,7 @@ import {
     deleteUserAccount,      
     getUserVideos, 
     deleteWatchHistory,
+    updateWatchHistory
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -70,6 +71,7 @@ router.route("/delete-account").delete(verifyJWT, deleteUserAccount);
 
 router.route("/c/:username/videos").get(getUserVideos);
 router.route("/delete-watch-history").delete(verifyJWT, deleteWatchHistory);
+router.route("/update-watch-history").patch(verifyJWT, updateWatchHistory);
 
 
 export default router;
